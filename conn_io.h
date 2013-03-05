d4796a93dabaed95bc9bd643570ed27c0a58416a
Tomas Pospisek <tpo_hp@sourcepole.ch>
2013-03-05 11:09:06 +0100

    german -> english comments

--- conn_io.h
+++ conn_io.h
@@ -1,8 +1,8 @@
 #ifndef __READLINE_H_
 #define __READLINE_H_
 /* 
- * identisch mit fgets, ausser, dass es auf Sockets funktioniert.
- * Siehe man 2 fgets
+ * A simple wrapper around "fgets". Read a newline terminated
+ * line from fd.  See 'man 2 fgets'
  */
 char *read_line(int socket_fd, char *s, int max_size);
 
